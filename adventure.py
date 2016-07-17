b200e60f89d13e40a7bd71a616e2481f02e3a405
Kevin Jiang <k.jiang@yale.edu>
2016-07-17 17:42:50 -0400

    commit all

---
+++ adventure.py
@@ -0,0 +1,5 @@
+def room1():
+	print "you are in a bathroom.  You can 1. eat cake or 2. throw the cake down the toilet."
+
+def cake_eaten():
+	print "you eat the cake.  Now, you can either, 3. leave the room or 4. stay in the room."
