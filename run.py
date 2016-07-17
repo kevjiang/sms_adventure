6b9cdf6547c6772fb92d3b012f57a975698e4e50
Kevin Jiang <k.jiang@yale.edu>
2016-07-17 14:07:28 -0400

    custom messages

--- run.py
+++ run.py
@@ -4,12 +4,35 @@ import os
 
 app = Flask(__name__)
 
+# @app.route("/", methods=['GET', 'POST'])
+# def hello_monkey():
+#     """Respond to incoming calls with a simple text message."""
+
+#     resp = twilio.twiml.Response()
+#     resp.message("Hello, Mobile Monkey")
+#     return str(resp)
+
+# Try adding your own number to this list!
+callers = {
+    "+17816402658": "Kevin",
+    "+14158675310": "Boots",
+    "+14158675311": "Virgil",
+}
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+    """Respond and greet the caller by name."""
+    print 'hi'
+    from_number = request.values.get('From', None)
+    print request.values
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
 
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message(message)
+
     return str(resp)
 
 if __name__ == "__main__":
