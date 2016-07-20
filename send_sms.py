1b7d9c41a7cc1b6b709761f7337a817d4f3066f8
Kevin Jiang <k.jiang@yale.edu>
2016-07-20 15:05:38 -0400

    added a send_sms message, heroku scheduler going to be added as well

--- send_sms.py
+++ send_sms.py
@@ -6,4 +6,4 @@ account_sid = "AC3b871b8dc7bb3785cddda6fd9ad271fe"
 auth_token = "6b65df96c48e7d3b99ac574d872af60a"
 client = TwilioRestClient(account_sid, auth_token)
 
-message = client.messages.create(to="+17816402658", from_="+17815705388", body="Hello there!")
+message = client.messages.create(to="+17816402658", from_="+17815705388", body="Hello there!  I just wanted to be friendly today...")
