9ebbce9c44cffe2f6f59017bd21b3d5f74ed6931
Kevin Jiang <k.jiang@yale.edu>
2016-07-20 22:29:17 -0400

    added search nytimes home page functionality

--- adventure.py
+++ adventure.py
@@ -1,11 +1,20 @@
+from nyt_scraper import *
+from time import *
+
 def response_handler(body):
     '''
     given text body string input
+    If body = "nytimes needle", searches nytimes for needle
+    Otherwise plays adventure game
     returns proper response message
     '''
     message = ""
 
-    if 'start' == body:
+    # prompt for a search of word on NYT
+    if "nytimes" in body.split():
+        total_nyt_count = nyt_all_text_count(body.split()[1])
+        message = "%s: nytimes.com homepage currently includes %d mentions of %s" % (strftime("%Y-%m-%d %I:%M:%S"), total_nyt_count, needle)
+    elif 'start' == body:
         message = classroom0()
     elif 'take a nap' == body:
         message = take_nap()
